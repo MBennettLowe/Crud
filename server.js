0aff62c537e9479d1aeac95b9bc7c104ac0560b7
MBennettLowe <mbennettlowe@gmail.com>
2020-06-23 17:35:09 -0400

    DB decl error

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-// Imported all Node modules // node js express mysql Tutorial https://www.youtube.com/watch?v=QK3oZlaMgRQ&list=PLRfmxUfYVUp1ShQifYvvrRZKMxCPr00kd
-// Will substitute mySQL db for Mongodb
+// This project will create a basic simple application using Nodejs, MySQL, JQuery and Bootsrap
+// https://www.youtube.com/watch?v=QK3oZlaMgRQ&list=PLRfmxUfYVUp1ShQifYvvrRZKMxCPr00kd
+// Using the CRUD technologies to CREATE, READ, UPDATE and DELETE Events
+
+// Will substitute Mongodb for mySQL 
 
 // Imported all Node modules 
 // Core Node modules
@@ -14,13 +17,12 @@ var bodyParser = require('body-parser');
 // It will parse all form data in JSON Object
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// used for formatting dates
+// Date Format Module - used for formatting dates
 var dateFormat = require('dateformat');
 var now = new Date();
 
 // This is the view engine
-// Template parsing
-// We are using EJS types
+// Template parsing - We are using EJS types
 app.set('view engine', 'ejs');
 
 // Gives access to Bootstrap
@@ -29,55 +31,40 @@ app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/crud"; // Connection URL
+const db = client.db("crud")
 
-// ******Start a MongoDB Server*********
-
-// Download the right MongoDB version from MongoDB
-// Create a database directory (in this case under /data).
-// Install and start a mongod process.
-// mongod --dbpath=/data
-// You should see the mongod process start up and print some status information.
-
-// *******Connect to MongoDB************
-
-// Create a new .js file and add the following code to try out some 
-// basic CRUD operations using the MongoDB driver.
-
-// Add code to connect to the server and the database crud:
-
-//const MongoClient = require('mongodb').MongoClient;
-//const assert = require('assert');
- 
-// Connection URL
+// Create a URL to the MongoDB local server.
 const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = "crud";
+
+// *******Connect to MongoDB************https://flaviocopes.com/node-mongodb/
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
-  console.log("Connected successfully to server");
+  console.log("Mongo Heyyyyy");
  
-  const db = client.db(dbName);
+  // Select a database using the client.db() method:
+  const db = client.db("crud")
  
   client.close();
 });
 
 // ****Global site title and base URL ********
 
-const siteTitle = "Simple Application"
-const baseURL = "http://localhost:4001/"
+// Site Title. The Site Title appears across the top of every page of a site. ... The site title might be the name of your company or organization, or a brief description of the organization, or a combination of the two.
+// A base URL is, basically, the consistent part of your web address.  Everything that follows it is known as a URL path.
+
+const siteTitle = "Simple Application" 
+const baseURL = "http://localhost:4000/"
 
 // Linking the Home Page by providing a Root
 
 // When page is loaded
-// Default page is loaded and the data is being called from Mongo database
+// Default page ('/') is loaded and the data is being called from Mongo database
 // We also adding some JS and CSS styles
 
 app.get('/', function(req, res) {
+
 
   // Get the event list
 
@@ -93,6 +80,26 @@ app.get('/', function(req, res) {
 // raw - driver returns documents as bson binary Buffer objects, default:false
 // callback has two parameters - an error object (if an error occured) and a cursor object.
 
+// function getData() {
+//   db.collection("e_events"),find({}, {_id:0}).sort({e_start_date:1})(function (err, document) {
+//   if (err) throw err;
+//   // do something
+//   )};
+
+
+// var dataset = [
+//   {// doint more stuff here.
+//   }
+// ];
+// });
+
+// mongodb.connect() is called mongodb.connect() is called asynchronouslyasynchronously
+// http.get("/crud", function(req, res) {
+//   MongoClient.connect(url_monitor, function (err, db) {
+//     assert.equal(null, err);
+//   });
+// });
+
   db.collection.find({}, {_id:0}).sort({e_start_date:1})(function (err, document) {
     res.render('pages/index', {
       siteTitle : siteTitle,
@@ -101,6 +108,7 @@ app.get('/', function(req, res) {
     });
   });
 });
+
 
 // con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, res) {
 //   res.render('pages/index', {
@@ -127,34 +135,8 @@ app.get('/', function(req, res) {
     // database: "mydb"
 //});
 
-// Core Node modules
-var express = require('express');
-var http = require('http');
-var mongodb = require('mongodb');
-var app = express(); // creating an express instance
-var bodyParser = require('body-parser');
-
-// bodyParser is simply used to get output from the form
-// It will parse all form data in JSON Object
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// used for formatting dates
-var dateFormat = require('dateformat');
-var now = new Date();
-
-// This is the view engine
-// Template parsing
-// We are using EJS types
-app.set('view engine', 'ejs');
-
-// Gives access to Bootstrap
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
 // Creating Server
-var server = app.listen(4000, function() {
-    console.log("Server started on 4000....");
-});
+var server = app.listen(4000, () => 
+    console.log("Server started on 4000...."),
+);
 
