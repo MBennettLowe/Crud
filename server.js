05dc3afecf286e2b1e307b04020f680506179719
MBennettLowe <mbennettlowe@gmail.com>
2020-07-04 15:49:29 -0400

    index.ejs error free

--- server.js
+++ server.js
@@ -4,26 +4,25 @@
 
 // Will substitute Mongodb for mySQL 
 
-// Imported all Node modules 
-// Core Node modules
+// Core Node modules and dependencies
 var express = require('express');
+var app = express(); // creating an express instance
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient,format = require('util').format;
 const assert = require('assert'); // still trying to research what this requirement does!!!
-var app = express(); // creating an express instance
-var bodyParser = require('body-parser');
 
-// bodyParser is simply used to get output from the form
-// It will parse all form data in JSON Object
+var bodyParser = require('body-parser');// bodyParser is simply used to get JSON output from the form.
+var db; // initialize db
+const ejsLint = require('ejs-lint');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Date Format Module - used for formatting dates
 var dateFormat = require('dateformat');
 var now = new Date();
 
-// This is the view engine
-// Template parsing - We are using EJS types
-app.set('view engine', 'ejs');
+// This is the view engine. Template parsing - We are using EJS types
+app.set("view engine", "ejs");
 
 // Gives access to Bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
@@ -31,21 +30,21 @@ app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-const db = client.db("crud")
-
 // Create a URL to the MongoDB local server.
 const url = 'mongodb://localhost:27017';
 
-// *******Connect to MongoDB************https://flaviocopes.com/node-mongodb/
+// *******Connect to MongoDB************
+// https://flaviocopes.com/node-mongodb/
 
-// Use connect method to connect to the server
+//var db; 
 MongoClient.connect(url, function(err, client) {
+  database = db;
   assert.equal(null, err);
   console.log("Mongo Heyyyyy");
  
   // Select a database using the client.db() method:
-  const db = client.db("crud")
- 
+   var db = client.db("crud")
+ // https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html
   client.close();
 });
 
@@ -63,6 +62,7 @@ const baseURL = "http://localhost:4000/"
 // Default page ('/') is loaded and the data is being called from Mongo database
 // We also adding some JS and CSS styles
 
+// home page request handler
 app.get('/', function(req, res) {
 
 
@@ -93,22 +93,24 @@ app.get('/', function(req, res) {
 // ];
 // });
 
-// mongodb.connect() is called mongodb.connect() is called asynchronouslyasynchronously
+// mongodb.connect() is called mongodb.connect() is called asynchronously
 // http.get("/crud", function(req, res) {
 //   MongoClient.connect(url_monitor, function (err, db) {
 //     assert.equal(null, err);
 //   });
 // });
 
-  db.collection.find({}, {_id:0}).sort({e_start_date:1})(function (err, document) {
-    res.render('pages/index', {
-      siteTitle : siteTitle,
-      pageTitle : "Event list",
-      items : result
-    });
+  //db.collection.find({}, {_id:0}).sort({e_start_date:1})(function (err, document) {
+  //   res.render('pages/index');
+  // });
+
+  res.render('pages/index', {
+    siteTitle : siteTitle,
+    pageTitle : "Event list",
+    items : '' 
+    
   });
 });
-
 
 // con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, res) {
 //   res.render('pages/index', {
@@ -136,6 +138,7 @@ app.get('/', function(req, res) {
 //});
 
 // Creating Server
+// initializes request listener
 var server = app.listen(4000, () => 
     console.log("Server started on 4000...."),
 );
